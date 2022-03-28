609be7555d57c1b9af87d5f72ae97b3ada96b325
kepisqo <52206026+kepisqo@users.noreply.github.com>
2022-03-28 11:29:57 +0300

    Update database.js

--- utility/database.js
+++ utility/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-app', 'root', '123*Ssk*06', {
+const sequelize = new Sequelize('node', 'root', '123*Ssk*06', {
     dialect: 'mysql',
     host: 'localhost',
     operatorsAliases: false
